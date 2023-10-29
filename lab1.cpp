58b616ee5f97d560df9830ca133d74b5194ce576
Aleksey <149272010+lipllleon@users.noreply.github.com>
2023-10-29 11:51:59 +0200

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int S;
+    float h;
+    cout << "Укажите площадь основания пирамиды: ";
+    cin >> S;
+    cout << "Укажите высоту пирамиды: ";
+    cin >> h;
+    cout << "Объём пирамиды: " << (S * h) / 3;
+}
