6b77c9993e93bddc9e877a023a19f7b2f4fb8957
Aleksey <149272010+lipllleon@users.noreply.github.com>
2024-07-17 21:24:59 +0200

    papki

---
+++ sem1/lab6.cpp
@@ -0,0 +1,261 @@
+﻿// 4 Вариант
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int strlength(string s) {
+    int l = s.length();
+    for (int i = 0; i < s.length(); i++)
+        if (s[i] == '-')
+            l--;
+    return l;
+}
+
+int main()
+{
+    system("chcp 1251 >> null");
+    setlocale(0, "RU");
+
+    cout << endl << endl << "__________Пункт 1___________" << endl << endl;
+
+    char str[101] = "Анна Павловна почти закрыла глаза в знак того, никто не может судить за то, что нравится императрице";
+    char dict[22] = "бвгджзйклмнпрстфxцчшщ";
+    bool ifsogl;
+    int k = 0;
+    cout << "Исходная строка: " << endl << str << endl << endl;
+    for (int i = 0; i < 101; i++) {
+        ifsogl = false;
+        for (int j = 0; j < 21; j++) {
+            if ((str[i] == dict[j]) || (str[i] == (char)((int)dict[j] - 32))) {
+                ifsogl = true;
+                break;
+            }
+        }
+        if (!ifsogl) {
+            str[k] = str[i];
+            k++;
+        }
+    }
+    for (int i = k; i < 101; i++)
+        str[i] = '\0';
+
+    cout << "Строка без согласных: " << endl << str;
+
+    //Таблица символов
+    cout << endl << endl;
+    for (int i = -64; i < -58; i++) {
+        cout << (char)i << " (" << i << ")" << "     ";
+        cout << (char)(i + 32) << " (" << i + 32 << ")" << endl;
+    }
+    cout << (char)-88 << " (" << -88 << ")" << "     ";
+    cout << (char)-72 << " (" << -72 << ")" << endl;
+    for (int i = -58; i < -32; i++) {
+        cout << (char)i << " (" << i << ")" << "     ";
+        cout << (char)(i + 32) << " (" << i + 32 << ")" << endl;
+    }
+    cout << '.' << " (" << (int)'.' << ")" << "     ";
+    cout << ',' << " (" << (int)',' << ")" << endl;
+    cout << '!' << " (" << (int)'!' << ")" << "     ";
+    cout << '?' << " (" << (int)'?' << ")" << endl;
+    cout << '(' << " (" << (int)'(' << ")" << "     ";
+    cout << ')' << " (" << (int)')' << ")" << endl;
+    cout << '"' << " (" << (int)'"' << ")" << "     ";
+    cout << '-' << " (" << (int)'-' << ")" << endl;
+    cout << ':' << " (" << (int)':' << ")" << "     ";
+    cout << ';' << " (" << (int)';' << ")" << endl << endl;
+
+    //_________________________________________________________________________________________________________________________
+
+    cout << endl << endl << "__________Пункт 2___________" << endl << endl;
+
+    //Чтение заданного слова----------------------------------------------
+    short N;
+    string wrd;
+    ifstream fin("input.txt");
+    //В файле input.txt данные хранятся в формате число пробел строчка (10 Прекрасный)
+    if (fin.is_open())
+        fin >> N >> wrd;
+    else {
+        cout << "Файл input.txt не открыт. Введите слово: ";
+        cin >> wrd;
+        cout << "Введите N: ";
+        cin >> N;
+    }
+    if (N < 0 || N > 10) {
+        cout << "Введён некорректный N (" << N << ") \nУстановлено число N = 10 \n";
+        N = 10;
+    }
+    else
+        cout << "N = " << N << endl;
+    cout << "Заданное слово: " << wrd;
+
+    //Удаление повторяющихся букв (из "АБВвбабвабб" получаем "абв")-------
+    string  rule = wrd;
+    int index = 0;
+    for (int i = 0; i < wrd.length(); i++) {
+        wrd[i] = tolower(wrd[i]);
+        int j;
+        for (j = 0; j < i; j++)
+            if (wrd[i] == wrd[j])
+                break;
+        if (i == j)
+            rule[index++] = wrd[i];
+    }
+    rule.erase(index, wrd.length() - index);
+    cout << " (" << rule << ")" << endl << endl;
+
+    //Получение массива слов----------------------------------------------
+    string s;
+    string result[10];
+    bool is_rule, is_similar;
+    ifstream file("text.txt");
+    if (!file.is_open()) {
+        cout << "Файл text.txt не открыт";
+        return -1;
+    }
+
+    while (!file.eof()) {
+        file >> s;
+        is_rule = true;
+        //Удаляем все символы, неявляющиеся русскими буквами, по краям слова.
+        //s.back() возвращает последний символ строки.
+        while (!((s[0] >= 'А') && (s[0] <= 'я') || (s[0] == 'Ё') || (s[0] == 'ё') || (s[0] == '\0')) || !((s[0] == '\0') || (s.back() >= 'А') && (s.back() <= 'я') || (s.back() == 'Ё') || (s.back() == 'ё')))
+        {
+            if (!((s[0] >= 'А') && (s[0] <= 'я') || (s[0] == 'Ё') || (s[0] == 'ё')))
+                s.erase(0, 1);
+            else
+                s.erase(s.length() - 1, 1);
+        }
+        //Проверка условия-------------------------------------
+        for (int i = 0; i < s.length(); i++) {
+            s[i] = tolower(s[i]);
+            //Проверка на наличие заданных букв------------
+            for (int j = 0; j < rule.length(); j++) {
+                if (s[i] == rule[j]) {
+                    is_rule = false;
+                    break;
+                }
+            }
+            if (!is_rule)
+                break;      //Если встретили букву из заданного слова - переходим к след слову
+        }
+        //Заполнение массива------------------------------------
+        if (is_rule && !(s == "")) {
+            /*от*/
+            //Сравнение с наименьшим словом массива-------------
+            if (strlength(s) > strlength(result[0])) {
+                is_similar = false;
+
+                //Проверка на совпадение слов-------------------
+                for (int i = N - 1; i > 0; i--) { //Идём справа налево
+                    if (result[i] == "")
+                        break;  //Если ячейка пуста - останавливаем проверку
+                    if (s == result[i]) {
+                        is_similar = true;
+                        break;
+                    }
+                }
+                if (!is_similar) {
+                    result[0] = s;
+
+                    //Сортировка по возрастанию длины------------
+                    for (int i = 0; i < N - 1; i++)
+                        if (strlength(result[i]) > strlength(result[i + 1]))
+                            swap(result[i], result[i + 1]);
+                        else
+                            break;
+                }
+            }
+            /*до*/
+
+            /*
+            for (int i = N - 1; i >= 0; i--) {
+                if (s == result[i])
+                    break;
+                if (s.length() > result[i].length()) {
+                    for (int j = 0; j < i; j++) {
+                        result[j] = result[j + 1];
+                    }
+                    result[i] = s;
+                    break;
+                }
+            }
+            */
+
+            /*
+            int min_l = INT16_MAX;
+            int min_i;
+            is_similar = false;
+            for (int i = 0; i < N; i++) {
+                if (result[i].length() < min_l) {
+                    min_l = result[i].length();
+                    min_i = i;
+                }
+                if (s == result[i]) {
+                    is_similar = true;
+                    break;
+                }
+            }
+            if (!is_similar) {
+                if (s.length() > min_l)
+                    result[min_i] = s;
+            }
+            //Для этого способа неоюходима сортировка перед выводом в result.txt и консоль
+            */
+
+            //Часть между от и до можно менять на одну из частей в комментариях
+            //Это иные способы заполнения массива)
+            //За объяснением можно в лс t.me/lipllleon
+        }
+    }
+    file.close();
+
+    //Вывод в result.txt---------------------------------------------------
+    ofstream fout("result.txt");
+    if (fout.is_open()) {
+        cout << "Файл открыт" << endl;
+
+        //Вывод по невозрастанию-----------
+        for (int i = N - 1; i >= 0; i--)
+            fout << N - i << ") " << result[i] << " (" << strlength(result[i]) << ")" << endl;
+        cout << "Данные записаны" << endl << endl << "Результат: " << endl;
+    }
+    else {
+        cout << "Файл не открыт" << endl << endl << "Результат: " << endl;
+    }
+    fout.close();
+
+    //Вывод в консоль-------------------------------------------------------
+    for (int i = N - 1; i >= 0; i--)
+        cout << N - i << ") " << result[i] << " (" << strlength(result[i]) << ")" << endl;
+
+    //_________________________________________________________________________________________________________________________
+
+    //Недоделан :(
+
+    cout << endl << endl << "__________Доп___________" << endl << endl;
+
+    ifstream filedop("textdop.txt");
+    ofstream output("output.txt");
+    string st;
+    int l;
+    while (!filedop.eof()) {
+        filedop >> st;
+        l = st.length();
+        for (int i = 0; i < l - 1; i++) {
+            if ((st[i] >= -64) && (st[i] <= -1) || (st[i] == -72) || (st[i] == -88)) {
+                if (st[i] == st[i + 1]) {
+                    st[i] = toupper(st[i]);
+                    st[i + 1] = toupper(st[i + 1]);
+                    st = st + "(" + st[i] + st[i + 1] + ")";
+                }
+            }
+        }
+        output << st << " ";
+    }
+    filedop.close();
+    output.close();
+    cout << "Отформатированный текст записан в output.txt" << endl << endl;
+}
---
+++ sem1/lab8.cpp
@@ -0,0 +1,214 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int K, S, var;
+    int* A;
+    bool flag;
+    cout << "Введите номер варианта: ";
+    cin >> var;
+    if (var < 1 || var > 7) {
+        cout << "Введён неверный номер варианта...\nvar = 4";
+        var = 4;
+    }
+    cout << "Введите размер массива K: ";
+    cin >> K;
+    A = (int*)malloc(K * sizeof(int));
+    for (int i = 0; i < K; i++) {
+        A[i] = i;
+        cout << A[i] << " ";
+    }
+
+    cout << "\nВведите изменение размера массива S: ";
+    cin >> S;
+
+    if (K + S < 0) {
+        cout << "Введено некорректное число S";
+    } 
+    else {
+        A = (int*)realloc(A, (K + S) * sizeof(int));
+        if (S < 0) {
+            cout << "Массив урезан: ";
+            for (int i = 0; i < K + S; i++)
+                cout << A[i] << " ";
+        }
+        else {
+            cout << "Введите элементы матрицы через пробел: \n";
+            for (int i = 0; i < K; i++)
+                cout << A[i] << " ";
+            for (int i = K; i < K + S; i++)
+                cin >> A[i];
+            //var = 3
+            int temp;
+            //var = 4
+            int maxi, max = 0;
+            //var = 5
+            int count1 = 0;
+            int count0 = 0;
+            switch (var)
+            {
+            case 1:
+                for (int i = K; i < K + S; i++) {
+                    if (A[i] == 0) {
+                        for (int j = 0; j < K + S; j++)
+                            if (A[j] % 2 == 0)
+                                A[j] = 0;
+                        break;
+                    }
+                }
+                break;
+            case 2:
+                for (int i = K; i < K + S - 1; i++)
+                    if (A[i] == A[i + 1] && (i + 2) < (K + S))
+                        for (int j = i + 2; j < K + S; j++)
+                            A[j] = 0;
+                break;
+            case 3:
+                temp = 0;
+                for (int i = K; i < K + S; i++) {
+                    if (A[i] > 0)
+                        temp++;
+                    if (temp > 3) {
+                        for (int j = K; j < K + S; j++)
+                            if (A[j] < 0)
+                                A[j] *= -1;
+                        break;
+                    }
+                }
+                break;
+            case 4:
+                for (int i = K; i < K + S; i++)
+                    if (A[i] > max) {
+                        max = A[i];
+                        maxi = i;
+                    }
+                if (A[maxi] % 2 == 1)
+                    A[maxi] *= 100;
+                break;
+            case 5:
+                for (int i = K; i < K + S; i++)
+                    if (A[i] % 2)
+                        count1++;
+                    else
+                        count0++;
+                if (count0 > count1)
+                    for (int i = 0; i < K + S; i++)
+                        A[i]++;
+                break;
+            case 6:
+                for (int i = K; i < K + S; i++)
+                    if (A[i] < 0) {
+                        for (int j = 0; j < K + S; j++)
+                            A[j] += 5;
+                        break;
+                    }
+                break;
+            case 7:
+                flag = false;
+                for (int i = K; i < K + S; i++)
+                    if (A[i] == 1)
+                        flag = true;
+                if (!flag)
+                    A[K + S - 1] = 999;
+                break;
+            default:
+                break;
+            }
+            for (int i = 0; i < K + S; i++)
+                cout << A[i] << " ";
+        }
+    }
+    free(A);
+
+    int N, M;
+    cout << "\n\nВведите размеры матрицы N x M: ";
+    cin >> N >> M;
+
+    int** AA;
+    AA = new int* [N];
+    for (int i = 0; i < N; ++i)
+        AA[i] = new int[M];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            AA[i][j] = 10*(i + 1) + j + 1;
+        }
+    }
+
+    cout << "Исходная матрица: \n";
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << AA[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    //матричная норма 
+    int maxsum = 0;
+    int sum = 0;
+    for (int j = 0; j < M; j++) {
+        sum = 0;
+        for (int i = 0; i < N; i++)
+            sum += abs(AA[i][j]);
+        if (sum > maxsum)
+            maxsum = sum;
+    }
+    int norm1 = maxsum;
+    cout << "Матричная норма исходной матрицы = " << norm1 << endl;
+
+    //транспонирование
+    int** B = new int* [M];
+    for (int i = 0; i < M; i++)
+        B[i] = new int[N];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            B[i][j] = AA[j][i];
+        }
+    }
+
+    cout << "Транспонированная матрица: \n";
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << B[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    maxsum = 0;
+    for (int j = 0; j < N; j++) {
+        sum = 0;
+        for (int i = 0; i < M; i++)
+            sum += abs(B[i][j]);
+        if (sum > maxsum)
+            maxsum = sum;
+    }
+    int norm2 = maxsum;
+    cout << "Матричная норма транспонированной матрицы = " << norm2 << endl;
+
+    if (norm2 > norm1)
+        cout << "Норма транспонированной матрицы больше исходной (" << norm2 << " > " << norm1 << ")\n";
+    else if (norm1 > norm2)
+        cout << "Норма исходной матрицы больше транспонированной (" << norm1 << " > " << norm2 << ")\n";
+    else
+        cout << "Нормы равны.\n";
+
+    for (int i = 0; i < N; i++)
+        delete[] AA[i];
+    delete[] AA;
+    for (int i = 0; i < M; i++)
+        delete[] B[i];
+    delete[] B;
+    return 0;
+}
