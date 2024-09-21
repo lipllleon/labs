19b68564e6158f907e3646e65f35359efc702728
Aleksey <149272010+lipllleon@users.noreply.github.com>
2024-09-21 23:32:05 +0200

    lab1 rework

--- sem3/proga/lab1.cpp
+++ sem3/proga/lab1.cpp
@@ -1,12 +1,15 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 const double pi = acos(-1);
 
 struct answer {
+private:
   double M;
   int count;
   double E;
 
+public:
   answer() = default;
   answer(double mid, int c, double ep) {
     M = mid;
@@ -20,57 +23,45 @@ struct answer {
   }
 };
 
-double getValue(double x) {
-  // double value = 0.8 * (x + 2) - 3 * cos(0.7 * x) + 2;
-  // double value = 0.7 * (sin(x / 2) + cos(2 * x)) - x;
-  double value = cos(2 * x + pi / 2) + x + 8;
-  return value;
-}
+double f1(double x) { return 0.8 * (x + 2) - 3 * cos(0.7 * x) + 2; }
+double f2(double x) { return 0.7 * (sin(x / 2) + cos(2 * x)) - x; }
+double f4(double x) { return cos(2 * x + pi / 2) + x + 8; }
 
-double getMidPoint(double L, double R) {
-  double M = (L + R) / 2;
+double getMidPoint(double L, double R) { return (L + R) / 2; }
 
-  return M;
-}
-
-bool isDifferent(double L, double R) {
-  if (getValue(L) * getValue(R) < 0)
+bool isDifferent(double (*f)(double), double L, double R) {
+  if (f(L) * f(R) < 0)
     return true;
   else
     return false;
 }
 
-bool inE(double M, double E) {
-  if (abs(getValue(M)) < E)
-    return true;
-  else
-    return false;
-}
-
-answer HalfDivision(double L, float step, float E) {
+answer HalfDivision(double (*f)(double)) {
+  double L = -10;
+  float step = 0.5;
   double R = L + step;
+  float E = 0.001;
   int count = 0;
-  while (!isDifferent(L, R)) {
+
+  while (!isDifferent(f, L, R)) {
     L += step;
     R += step;
   }
   double M = getMidPoint(L, R);
-  while (!inE(M, E)) {
-    if (isDifferent(L, M))
+
+  while (abs(f(M)) > E) {
+    if (isDifferent(f, L, M))
       R = M;
     else
       L = M;
     M = getMidPoint(L, R);
     count++;
   }
-  return answer(M, count, abs(getValue(M)));
+
+  return answer(M, count, abs(f(M)));
 }
 
 int main() {
-  setlocale(0, "ru");
-  double L;
-  std::cout << "Введите левую границу" << std::endl;
-  std::cin >> L;
-  answer result = HalfDivision(L, 0.5, 0.001);
+  answer result = HalfDivision(f1);
   result.print();
 }
