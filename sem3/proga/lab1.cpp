2524f527d0d65f2284e0ddea3fd5d00733f4a7cc
Aleksey <149272010+lipllleon@users.noreply.github.com>
2024-09-28 13:26:06 +0200

    lab2 -> lab1 proga

--- sem3/proga/lab1.cpp
+++ sem3/proga/lab1.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <sstream>
+#include <string>
 
 const double pi = acos(-1);
 
@@ -20,6 +22,20 @@ public:
     std::cout << "Кол-во итераций: " << count << std::endl;
     std::cout << "Погрешность: " << E << std::endl;
   }
+  std::string printTable() {
+    std::string space = ", ";
+    std::ostringstream Mstring;
+    Mstring << M;
+    std::string Mstr = Mstring.str();
+    std::ostringstream Estring;
+    Estring << E;
+    std::string Estr = Estring.str();
+    std::ostringstream Cstring;
+    Cstring << count;
+    std::string Cstr = Cstring.str();
+    std::string s = Mstr + space + Cstr + space + Estr;
+    return s;
+  }
 };
 
 double f1(double x) { return 0.8 * (x + 2) - 3 * cos(0.7 * x) + 2; }
@@ -28,6 +44,10 @@ double f4(double x) { return cos(2 * x + pi / 2) + x + 8; }
 
 double getMidPoint(double L, double R) { return (L + R) / 2; }
 
+double getInterPoint(double (*f)(double), double L, double R) {
+  return (L * f(R) - R * f(L)) / (f(R) - f(L));
+}
+
 bool isDifferent(double (*f)(double), double L, double R) {
   if (f(L) * f(R) < 0)
     return true;
@@ -35,17 +55,21 @@ bool isDifferent(double (*f)(double), double L, double R) {
     return false;
 }
 
-answer HalfDivision(double (*f)(double)) {
-  double L = -10;
-  float step = 0.5;
+double getLeftPoint(double (*f)(double), double start, float step) {
+  double L = start;
   double R = L + step;
-  float E = 0.001;
-  int count = 0;
-
-  while (!isDifferent(f, L, R) && abs(f(L)) < 100) {
+  while (!isDifferent(f, L, R)) {
     L += step;
     R += step;
   }
+  return L;
+}
+
+answer halfDivisionMethod(double (*f)(double), double E) {
+  int count = 1;
+  float step = 0.5;
+  double L = getLeftPoint(f, -10, step);
+  double R = L + step;
   double M = getMidPoint(L, R);
 
   while (abs(f(M)) > E && count < 100) {
@@ -60,7 +84,42 @@ answer HalfDivision(double (*f)(double)) {
   return answer(M, count, abs(f(M)));
 }
 
+answer secantMethod(double (*f)(double), double E) {
+  int count = 1;
+  float step = 0.5;
+  double L = getLeftPoint(f, -10, 0.5);
+  double R = L + step;
+  double M = getInterPoint(f, L, R);
+
+  while (abs(f(M)) > E && count < 100) {
+    if (isDifferent(f, L, M))
+      R = M;
+    else
+      L = M;
+    M = getInterPoint(f, L, R);
+    count++;
+  }
+
+  return answer(M, count, abs(f(M)));
+}
+
 int main() {
-  answer result = HalfDivision(f1);
-  result.print();
+  setlocale(0, "RU");
+  float E = 0.001;
+  halfDivisionMethod(f1, E).print();
+  secantMethod(f1, E).print();
+
+  std::cout << "              "
+            << "E = 0.1"
+            << "                       "
+            << "E = 0.01"
+            << "                         "
+            << "E = 0.0001" << std::endl;
+  std::cout << "Метод пол.дел." << halfDivisionMethod(f2, 0.1).printTable()
+            << "          " << halfDivisionMethod(f2, 0.01).printTable()
+            << "           " << halfDivisionMethod(f2, 0.0001).printTable()
+            << std::endl;
+  std::cout << "Метод сек.    " << secantMethod(f2, 0.1).printTable()
+            << "       " << secantMethod(f2, 0.01).printTable() << "          "
+            << secantMethod(f2, 0.0001).printTable() << std::endl;
 }
